93707bc008970a7fd039f4a6d8d8953463a6e67d
EvaDupont <evadpnt@gmail.com>
2021-04-16 10:50:56 +0200

    Récap panier responsive ok

--- Front_end/app.js
+++ Front_end/app.js
@@ -172,6 +172,22 @@ ajoutPanier = () => {
 
 /************** PAGE PANIER **************/
 
+/*Vérification du panier*/
+checkPanier = () => {
+  /*Vérifier qu'il y ait au moins un produit dans le panier*/
+  let etatPanier = JSON.parse(localStorage.getItem("panier"));
+  /*Si le panier est vide ou null*/
+  if  (etatPanier.length < 1 || etatPanier == null) {
+    alert("Votre panier est vide");
+    return false;
+  } else {
+    console.log("Le panier n'est pas vide");
+    return true;
+  }
+};
+
+/*création du tableau panier */
+
 panierCreation = () => {
   if (panier.length > 0) {
     document.getElementById("panierVide").remove();
@@ -198,14 +214,6 @@ panierCreation = () => {
     ligneTableau.appendChild(recapPrixUnitaire);
     ligneTableau.appendChild(recapRemove);
 
-    /* contenu des entetes */
-
-    recapPhoto.textContent = "Article";
-    recapNom.textContent = "Nom";
-    recapPrixUnitaire.textContent = "Prix";
-    recapRemove.textContent = "Supprimer";
-
-  
     /* Boucle FOR pour affichage des articles dans le panier*/ 
      
     for (let i = 0; i<panier.length; i++) {
@@ -226,8 +234,7 @@ panierCreation = () => {
       photoArticle.setAttribute("src", panier[i].imageUrl);
       photoArticle.setAttribute("alt", "Photo de l'article commandé");
       removeArticle.setAttribute("id", "remove" + [i]);
-      removeArticle.setAttribute("class", "fas fa-times-circle fa-3x circle");
-      removeArticle.setAttribute("title", "Supprimer");
+      removeArticle.setAttribute("class", "fas fa-times-circle fa-2x circle");
 
       console.log(i);
 
@@ -251,7 +258,6 @@ panierCreation = () => {
       console.log(panier[i].name);
       
     };
-
 
     /* Dernière ligne du tableau : Total */
 
@@ -279,6 +285,9 @@ panierCreation = () => {
   }
 };
 
+
+
+
 /* supprimer un produit */
 
 annulerArticle = (i) => {
@@ -290,5 +299,247 @@ annulerArticle = (i) => {
   window.location.reload();
 };  
 
+/************************ FORMULAIRE DE COMMANDE *********************/
 
+/* => vérifie les inputs du formulaire */
+
+checkInput = () => {
+  /* Controle des données clients par la regex (ou expression régulière) : permet de vérifier la qualité des informations envoyées. */
+  let checkNumber = /[0-9]/;
+  let checkMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let checkSpecialCharacter = /[§!@#$%^&*().?":{}|<>]/;
+
+  /* message fin de controle */
+  let checkMessage = "";
+
+  /* Récupération des inputs */
+
+  let prenom = document.getElementById("firstName").value;
+  let nom = document.getElementById("lastName").value;
+  let adresse = document.getElementById("address").value;
+  let ville = document.getElementById("city").value;
+  let email = document.getElementById("email").value;
+  
+  /* tests des différents input du formulaire */
+
+ /*Test du prénom*/
+  if (
+    checkNumber.test(prenom) == true ||
+    checkSpecialCharacter.test(prenom) == true ||
+    prenom == ""
+  ) {
+    checkMessage = checkMessage + "\n" + "Veuillez vérifier les informations concernant votre prénom. Les caractères spéciaux ou les chiffres ne sont pas autorisés";
+  } else {
+    console.log("Prénom accepté");
+  }
+
+  /* Test du nom*/
+  if (
+    checkNumber.test(nom) == true ||
+    checkSpecialCharacter.test(nom) == true ||
+    nom == ""
+  ) {
+    checkMessage = "Veuillez vérifier les informations concernant votre nom. Les caractères spéciaux ou les chiffres ne sont pas autorisés";
+  } else {
+    console.log("Nom accepté");
+  }
+
+  /*Test de l'adresse*/
+  if (checkSpecialCharacter.test(adresse) == true || adresse == "") {
+    checkMessage = checkMessage + "\n" + "Veuillez vérifier les informations concernant votre adresse postale. Les caractères spéciaux ne sont pas autorisés";
+  } else {
+    console.log(" Adresse postale acceptée");
+  }
+
+  /*Test de la ville*/
+  if (
+    (checkSpecialCharacter.test(ville) == true ||
+      checkNumber.test(ville) == true) ||
+    ville == ""
+  ) {
+    checkMessage = checkMessage + "\n" + "Veuillez vérifier les informations concernant votre ville. Les caractères spéciaux ou les chiffres ne sont pas autorisés";
+  } else {
+    console.log("Ville acceptée");
+  }
+  
+  /*Test du mail*/
+  if (checkMail.test(email) == false) {
+    checkMessage = checkMessage + "\n" + "Veuillez vérifier les informations concernant votre email. Les caractères spéciaux ne sont pas autorisés";
+  } else {
+    console.log("Adresse mail acceptée");
+  }
+
+  /*Si un des champs n'est pas conforme => message d'alert avec la raison*/
+  if (checkMessage != "") {
+    alert("Attention certaines données ne sont pas conformes :" + "\n" + checkMessage);
+  }
+
+  /* Si le formulaire est validé => construction de l'objet contact */
+  else {
+    contact = {
+      lastName: nom,
+      firstName: prenom,
+      address: adresse,
+      city: ville,
+      email: email,
+    };
+    return contact;
+  }
+};
+
+/*Envoi du formulaire vérifié à l'API */
+
+/* Tableau et objet demandé par l'API pour la commande*/
+let contact;
+let products = [];
+let url = "http://localhost:3000/api/teddies/order";
+
+const envoiFormulaire = (sendForm, url) => {
+  return new Promise((resolve) => {
+    let request = new XMLHttpRequest();
+    request.onload = function () {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
+        sessionStorage.setItem("order", this.responseText);
+        window.location = "./confirmation.html";
+        resolve(JSON.parse(this.responseText));
+        console.log(sendForm);
+      } else {
+      }
+    };
+    request.open("POST", url);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(sendForm);
+    console.log(sendForm);
+  });
+};
+
+confirmCommande = () => {
+  let commander = document.getElementById("form_1");
+  commander.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    /*Si le panier n'est pas vide et que le formulaire est valide => Construction du tableau products envoyé à l'API*/
+    if (checkPanier() == true && checkInput() != null) {
+      console.log("L'envoi peut etre fait");
+      panier.forEach((article) => {
+        products.push(article._id);
+      });
+      console.log("Ce tableau sera envoyé à l'API : " + products);
+
+      /*Création de l'objet à envoyer*/
+      let commande = {
+        contact,
+        products,
+      };
+
+      let sendForm = JSON.stringify(commande);
+      envoiFormulaire(sendForm, url);
+      console.log(commande);
+
+      /* Une fois la commande effectuée retour à l'état initial des tableaux/objet/localStorage */
+      contact = {};
+      products = [];
+      localStorage.clear();
+    } else {
+      console.log("ERROR");
+    }
+  });
+};
+
+/*Récupération des informations pour affichage sur la page de confirmation*/
+retourOrder = () => {
+  if (sessionStorage.getItem("order") != null) {
+    let order = JSON.parse(sessionStorage.getItem("order"));
+    document.getElementById("firstName").innerHTML = order.contact.firstName;
+    document.getElementById("orderId").innerHTML = order.orderId;
+    console.log(order);
+    sessionStorage.removeItem("order");
+  }
+
+  /*Redirection vers l'accueil*/
+  else {
+    alert("Merci pour vote commande. A bientôt");
+    window.location = "./index.html";
+  }
+};
+
+/*Tableau de recap de la commande dans la page de confirmation*/
+
+confirmRecap = () => {
+
+  /*Création de la structure du tableau récapitulatif*/
+  let recapConfirm = document.createElement("table");
+  let ligneConfirm = document.createElement("tr");
+  let confirmPhoto = document.createElement("th");
+  let confirmNom = document.createElement("th");
+  let confirmPrixUnitaire = document.createElement("th");
+  let ligneConfirmTotal = document.createElement("tr");
+  let colonneConfirmTotal = document.createElement("th");
+  let confirmPrixPaye = document.createElement("td");
+
+  /*Placement de la structure dans la page*/
+  let confirmPanier = document.getElementById("confirmation-recap");
+  confirmPanier.appendChild(recapConfirm);
+  recapConfirm.appendChild(ligneConfirm);
+  ligneConfirm.appendChild(confirmPhoto);
+  ligneConfirm.appendChild(confirmNom);
+  ligneConfirm.appendChild(confirmPrixUnitaire);
+
+  /*contenu des entetes*/
+  confirmPhoto.textContent = "Article";
+  confirmNom.textContent = "Nom";
+  confirmPrixUnitaire.textContent = "Prix";
+
+  /*Incrémentation de l'id des lignes pour chaque produit*/
+  let i = 0;
+  let order = JSON.parse(sessionStorage.getItem("order"));
+
+  order.products.forEach((orderArticle) => {
+    /*Création de la ligne*/
+    let ligneConfirmArticle = document.createElement("tr");
+    let photoConfirmArticle = document.createElement("img");
+    let nomConfirmArticle = document.createElement("td");
+    let prixUnitConfirmArticle = document.createElement("td");
+
+    /*Attribution des class pour le css*/
+    ligneConfirmArticle.setAttribute("id", "article_acheté" + i);
+    photoConfirmArticle.setAttribute("class", "photo_article_acheté");
+    photoConfirmArticle.setAttribute("src", orderArticle.imageUrl);
+    photoConfirmArticle.setAttribute("alt", "Photo de l'article acheté");
+
+    /*Insertion dans le HTML*/
+    recapConfirm.appendChild(ligneConfirmArticle);
+    ligneConfirmArticle.appendChild(photoConfirmArticle);
+    ligneConfirmArticle.appendChild(nomConfirmArticle);
+    ligneConfirmArticle.appendChild(prixUnitConfirmArticle);
+
+    /*Contenu des lignes*/
+
+    nomConfirmArticle.textContent = orderArticle.name;
+    prixUnitConfirmArticle.textContent = orderArticle.price / 100 + "€";
+  });
+
+  /*Dernière ligne du tableau : Total*/
+  recapConfirm.appendChild(ligneConfirmTotal);
+  ligneConfirmTotal.appendChild(colonneConfirmTotal);
+  ligneConfirmTotal.setAttribute("id", "ligneSomme");
+  colonneConfirmTotal.textContent = "Total payé";
+  ligneConfirmTotal.appendChild(confirmPrixPaye);
+
+  confirmPrixPaye.setAttribute("id", "sommeConfirmTotal");
+  confirmPrixPaye.setAttribute("colspan", "4");
+  colonneConfirmTotal.setAttribute("id", "colonneConfirmTotal");
+  colonneConfirmTotal.setAttribute("colspan", "2");
+
+  /*Calcul de l'addition total*/
+  let sommeConfirmTotal = 0;
+  order.products.forEach((orderArticle) => {
+    sommeConfirmTotal += orderArticle.price / 100;
+  });
+
+  /*Affichage du prix total à payer dans l'addition */
+  console.log(sommeConfirmTotal);
+  document.getElementById("sommeConfirmTotal").textContent =
+    sommeConfirmTotal + " €";
+};
 
